0a92f48ca21d189e20976b317036757d0ed30631
RobertoMarchini <robertoasp2013@gmail.com>
2022-08-26 00:30:20 -0300

    inserção do primeiro script em Python

---
+++ DIO.me-Desafio-Git_GitHub-Primeiro_Repositorio/Scripts em Python/Elefantes.py
@@ -0,0 +1,23 @@
+def incomodam(n):
+    if n <= 0: return ""
+    if n == 1: return "incomodam "
+    return "incomodam " + incomodam(n - 1)
+
+def elefantes(n):
+    if n <= 0:
+        return ""
+
+    if n > 1:
+        if n-1 == 1:
+            sentenca_1 = "" 
+        else:
+            sentenca_1 = "\n" + str(n-1) + " elefantes incomodam muita gente"
+        sentenca_2 = "\n" + str(n) + " elefantes " + incomodam(n) + "muito mais"
+        return elefantes(n-1) + sentenca_1 + sentenca_2
+    else:
+        return "\nUm elefante incomoda muita gente"
+
+def main():
+    n = int(input("digite:"))
+    print(elefantes(n))
+main()
